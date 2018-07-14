edeec03f87f55a9774bc0167d4b7cec318eab49b
mjkim <eoqkrskfk94@naver.com>
2018-07-14 15:34:13 +0900

    업데이트

--- app/src/main/java/com/example/mjkim/wheelchair2/LoadingScreen.java
+++ app/src/main/java/com/example/mjkim/wheelchair2/LoadingScreen.java
@@ -3,11 +3,11 @@ package com.example.mjkim.wheelchair2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LoadingScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.Loading_screen);
     }
 }
