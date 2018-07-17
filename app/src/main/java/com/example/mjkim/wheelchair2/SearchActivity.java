f49bdc4379f9b9ac0b970a0fa22834d14278502a
Nam <jjune095@naver.com>
2018-07-17 19:45:57 +0900

    SearchActivity init

--- app/src/main/java/com/example/mjkim/wheelchair2/SearchActivity.java
+++ app/src/main/java/com/example/mjkim/wheelchair2/SearchActivity.java
@@ -1,13 +1,32 @@
 package com.example.mjkim.wheelchair2;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class SearchActivity extends AppCompatActivity {
+
+    ImageButton back_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        back_button = (ImageButton)findViewById(R.id.back_b);
+
+        back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openFirstScreen();
+            }
+        });
+    }
+    public void openFirstScreen(){
+        Intent intent = new Intent(this, FirstScreen.class);
+        startActivity(intent);
     }
 }
