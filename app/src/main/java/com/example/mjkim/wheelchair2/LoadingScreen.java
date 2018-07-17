7b634c3aa94988e406655743ccd341a5e70516a8
Nam <jjune095@naver.com>
2018-07-17 18:48:27 +0900

    Loading Screen add

--- app/src/main/java/com/example/mjkim/wheelchair2/LoadingScreen.java
+++ app/src/main/java/com/example/mjkim/wheelchair2/LoadingScreen.java
@@ -1,5 +1,8 @@
 package com.example.mjkim.wheelchair2;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.os.HardwarePropertiesManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,15 @@ public class LoadingScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loading_screen);
+
+        Handler handler =new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(LoadingScreen.this,FirstScreen.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000);
     }
 }
