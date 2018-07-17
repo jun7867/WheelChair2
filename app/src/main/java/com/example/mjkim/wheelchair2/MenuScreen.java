4fc8b17a3e30e4a4528bad277579218e23c332a8
mjkim <eoqkrskfk94@naver.com>
2018-07-17 18:16:26 +0900

    화면 전환

--- app/src/main/java/com/example/mjkim/wheelchair2/MenuScreen.java
+++ app/src/main/java/com/example/mjkim/wheelchair2/MenuScreen.java
@@ -4,30 +4,29 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
-public class FirstScreen extends AppCompatActivity {
+public class MenuScreen extends AppCompatActivity {
 
-    ImageButton menu_button;
+    ImageButton back_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.first_screen);
+        setContentView(R.layout.menu_screen);
 
-        menu_button = (ImageButton)findViewById(R.id.menu_b);
+        back_button = (ImageButton)findViewById(R.id.back_b);
 
-        menu_button.setOnClickListener(new View.OnClickListener() {
+        back_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openMenuTab();
+                openFirstScreen();
             }
         });
     }
 
-    public void openMenuTab(){
-        Intent intent = new Intent(this, MenuScreen.class);
+    public void openFirstScreen(){
+        Intent intent = new Intent(this, FirstScreen.class);
         startActivity(intent);
     }
 }
