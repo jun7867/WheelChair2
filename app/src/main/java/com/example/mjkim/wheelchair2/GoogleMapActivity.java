c474df1fa7369fe183e12be86c5452662985a3e0
heeseok <dldydldy@naver.com>
2018-07-30 16:50:14 +0900

    네이버 지도 리를빗

--- app/src/main/java/com/example/mjkim/wheelchair2/GoogleMapActivity.java
+++ app/src/main/java/com/example/mjkim/wheelchair2/GoogleMapActivity.java
@@ -1,8 +1,21 @@
 package com.example.mjkim.wheelchair2;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 
@@ -13,17 +26,41 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.security.MessageDigest;
+
 public class GoogleMapActivity extends AppCompatActivity implements OnMapReadyCallback {
+    //상단 버튼
     ImageButton back_button;
     ImageButton menu_button;
+
+    GoogleMap googleMap;
+
+    //gps관련
+    double mLatitude, mLongitude;
+    LocationManager locationManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_google_map);
 
-        SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        supportMapFragment.getMapAsync(this);
+        getAppKeyHash();
+
+//        SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+//        supportMapFragment.getMapAsync(this);
+
+        //boxMap = (RelativeLayout)findViewById(R.id.boxmap);
+
+        locationManager = (LocationManager)getSystemService(LOCATION_SERVICE);
+
+        //GPS가 켜져있는지 체크
+        if(!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            Intent intent1 = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            intent1.addCategory(Intent.CATEGORY_DEFAULT);
+            startActivity(intent1);
+            finish();
+        }
 
         back_button = (ImageButton)findViewById(R.id.back_b);
         menu_button = (ImageButton)findViewById(R.id.menu_b);
@@ -41,20 +78,117 @@ public class GoogleMapActivity extends AppCompatActivity implements OnMapReadyCa
                 openMenuTab();
             }
         });
+
+        //마시멜로 이상이면 권한 요청하기
+        if(Build.VERSION.SDK_INT >= 23){
+            //권한이 없는 경우
+            if(ContextCompat.checkSelfPermission(GoogleMapActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+                    ContextCompat.checkSelfPermission(GoogleMapActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(GoogleMapActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION , Manifest.permission.ACCESS_FINE_LOCATION} , 1);
+            }
+            //권한이 있는 경우
+            else{
+                requestMyLocation();
+            }
+        }
+        //마시멜로 아래
+        else{
+            requestMyLocation();
+        }
+
     }
 
-    //36.103245, 129.388664
+    private void getAppKeyHash() {
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md;
+                md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                String something = new String(Base64.encode(md.digest(), 0));
+                Log.d("Hash key", something);
+            }
+        } catch (Exception e) {
+// TODO Auto-generated catch block
+            Log.e("name not found", e.toString());
+        }
+    }
 
+    //구글맵 생성 콜백
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        LatLng HGU = new LatLng(36.103245, 129.388664);
+        this.googleMap = googleMap;
 
-        googleMap.addMarker(new MarkerOptions().position(HGU).title("HGU"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(HGU, 12));
+        //지도타입 - 일반
+        this.googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        //나의 위치 설정
+        LatLng myPosition = new LatLng(mLatitude , mLongitude);
+
+        //내위치 마커
+        googleMap.addMarker(new MarkerOptions().position(myPosition).title("Current Position"));
+
+        //화면중앙의 위치와 카메라 줌비율
+        this.googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myPosition, 12));
+
+
+
+        //지도 보여주기
+//        boxMap.setVisibility(View.VISIBLE);
+
+
+
+//        LatLng HGU = new LatLng(36.103245, 129.388664);
+//
+//        googleMap.addMarker(new MarkerOptions().position(HGU).title("HGU"));
+//        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(HGU, 12));
     }
 
+    //나의 위치 요청
+    public void requestMyLocation(){
+        if(ContextCompat.checkSelfPermission(GoogleMapActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(GoogleMapActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            return;
+        }
+        //요청
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 100, 10, locationListener);
+    }
+
+    //위치정보 구하기 리스너
+    LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            if(ContextCompat.checkSelfPermission(GoogleMapActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+                    ContextCompat.checkSelfPermission(GoogleMapActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                return;
+            }
+            //나의 위치를 한번만 가져오기 위해
+            locationManager.removeUpdates(locationListener);
+
+            //위도 경도
+            mLatitude = location.getLatitude();   //위도
+            mLongitude = location.getLongitude(); //경도
+
+            //맵생성
+            SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+            //콜백클래스 설정
+            mapFragment.getMapAsync(GoogleMapActivity.this);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) { Log.d("gps", "onStatusChanged"); }
+
+        @Override
+        public void onProviderEnabled(String provider) { }
+
+        @Override
+        public void onProviderDisabled(String provider) { }
+    };
+
+
+    //설정메뉴로 가기
     public void openMenuTab(){
-        Intent intent = new Intent(this, MenuScreen.class);
-        startActivity(intent);
+        Intent intent2 = new Intent(this, MenuScreen.class);
+        startActivity(intent2);
     }
 }
