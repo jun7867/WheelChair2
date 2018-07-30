4caefab253609b0c89df007a9b732ce3879d7ae6
Nam <jjune095@naver.com>
2018-07-30 15:51:10 +0900

    픽토그램 초기 버전

---
+++ app/src/main/java/com/example/mjkim/wheelchair2/MyAdapter.java
@@ -0,0 +1,50 @@
+package com.example.mjkim.wheelchair2;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+import com.example.mjkim.wheelchair2.R;
+
+class MyAdapter extends BaseAdapter {
+    Context context;
+    int layout;
+    int img[];
+    LayoutInflater inf;
+
+    public MyAdapter(Context context, int layout, int[] img) {
+        this.context = context;
+        this.layout = layout;
+        this.img = img;
+        inf = (LayoutInflater) context.getSystemService
+                (Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return img.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return img[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView==null)
+            convertView = inf.inflate(layout, null);
+        ImageView iv = (ImageView)convertView.findViewById(R.id.imageView1);
+        iv.setImageResource(img[position]);
+
+        return convertView;
+    }
+}
