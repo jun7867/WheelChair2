38d0f94a27fbaf42d7eec6d4200402e1d5c005ff
Nam <jjune095@naver.com>
2018-08-02 16:40:52 +0900

    login ++

--- app/src/main/java/com/example/mjkim/wheelchair2/FirstScreen.java
+++ app/src/main/java/com/example/mjkim/wheelchair2/FirstScreen.java
@@ -1,21 +1,50 @@
 package com.example.mjkim.wheelchair2;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.example.mjkim.wheelchair2.Login.LoginScreen;
 import com.example.mjkim.wheelchair2.Login.User;
 import com.example.mjkim.wheelchair2.naver_map_fragment.FragmentMapActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.io.FileInputStream;
 
 public class FirstScreen extends AppCompatActivity {
 
     ImageButton menu_button;
     ImageButton login_button;
     private BackPressCloseHandler backPressCloseHandler;
-
+    private FirebaseAuth mAuth; // 이메일 비밀번호 로그인 모듈변수
+    private FirebaseUser currentUser; //로그인 된 유저정보 변수
+    private TextView mTextMessage;
+//    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+//            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+//        @Override
+//        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+//            switch (menuItem.getItemId()) {
+//                case R.id.navigation_home:
+//                    mTextMessage.setText(currentUser.getEmail());
+//                    return true;
+//                case R.id.navigation_dashboard:
+//                    mTextMessage.setText(currentUser.getUid());
+//                    return true;
+//                case R.id.navigation_notifications:
+//                    mTextMessage.setText(currentUser.getEmail());
+//                    return true;
+//            }
+//
+//            return false;
+//        }
+//    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,11 +71,24 @@ public class FirstScreen extends AppCompatActivity {
 
         backPressCloseHandler = new BackPressCloseHandler(this);
 
-
-
-
+        mAuth=FirebaseAuth.getInstance();
+  //      mTextMessage=(TextView)findViewById(R.id.message);
+  //      BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+  //      navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
     }
+    //로그인 되어있으면 currentUser 변수에 유저정보 할당. 아닌경우 login 페이지로 이동!
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        currentUser = mAuth.getCurrentUser();
+        if(currentUser == null){
+            startActivity(new Intent(FirstScreen.this, LoginScreen.class));
+            finish();
+        }
+    }
+
 
     public void onClick(View view){
         if(view.getId()== R.id.surround_b){
--- app/src/main/java/com/example/mjkim/wheelchair2/Login/EmailActivity.java
+++ app/src/main/java/com/example/mjkim/wheelchair2/Login/EmailActivity.java
@@ -1,15 +1,94 @@
 package com.example.mjkim.wheelchair2.Login;
 
+import android.app.DialogFragment;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.mjkim.wheelchair2.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.auth.FirebaseUser;
 
 public class EmailActivity extends AppCompatActivity {
+    private EditText email;
+    private EditText name;
+    private EditText password;
+    private Button signup;
+ //   private DialogFragment progressDialog;
+    private FirebaseAuth mAuth; //로그인 변수
+    private FirebaseUser currentUser;  // 정보 담을거.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_email);
+        email = (EditText) findViewById(R.id.signupActivity_edittext_email);
+        name = (EditText) findViewById(R.id.signupActivity_edittext_name);
+        password = (EditText) findViewById(R.id.signupActivity_edittext_password);
+        signup = (Button) findViewById(R.id.signupActivity_button_signup);
+        mAuth = FirebaseAuth.getInstance();
+//        FirebaseAuth.getInstance()
+//                .createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+//                .addOnCompleteListener(EmailActivity.this, new OnCompleteListener<AuthResult>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<AuthResult> task) {
+//                        final String uid = task.getResult().getUser().getUid();
+//                        progressDialog.dismiss();
+//                        Toast.makeText(getApplicationContext(), "회원가입을 성공하셨습니다.", Toast.LENGTH_SHORT).show();
+//                        finish();
+//                    }
+//                });
+
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email1 = email.getText().toString();
+                String name1 = name.getText().toString();
+                String pw1 = password.getText().toString();
+                createUser(email1,name1,pw1);
+            }
+        });
+
+    }
+    private void createUser(String email,final String name, String password){
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(!task.isSuccessful()) {
+                            try {
+                                throw task.getException();
+                            } catch(FirebaseAuthWeakPasswordException e) {
+                                Toast.makeText(EmailActivity.this,"비밀번호가 간단해요.." ,Toast.LENGTH_SHORT).show();
+                            } catch(FirebaseAuthInvalidCredentialsException e) {
+                                Toast.makeText(EmailActivity.this,"email 형식에 맞지 않습니다." ,Toast.LENGTH_SHORT).show();
+                            } catch(FirebaseAuthUserCollisionException e) {
+                                Toast.makeText(EmailActivity.this,"이미존재하는 email 입니다." ,Toast.LENGTH_SHORT).show();
+                            } catch(Exception e) {
+                                Toast.makeText(EmailActivity.this,"다시 확인해주세요.." ,Toast.LENGTH_SHORT).show();
+                            }
+                        }else {
+
+                            currentUser = mAuth.getCurrentUser();
+
+                            Toast.makeText(EmailActivity.this, "가입 성공  ", Toast.LENGTH_SHORT).show();
+
+                            startActivity(new Intent(EmailActivity.this, LoginScreen.class));
+                            finish();
+                        }
+                    }
+                });
     }
 }
