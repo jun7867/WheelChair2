bbe709049fc033291c3234483ef146438b7c73ed
heeseok <dldydldy@naver.com>
2018-08-12 20:07:29 +0900

    마커 정보 프래그먼트 완료

--- app/src/main/java/com/example/mjkim/wheelchair2/StatsFragment.java
+++ app/src/main/java/com/example/mjkim/wheelchair2/StatsFragment.java
@@ -1,7 +1,18 @@
 package com.example.mjkim.wheelchair2;
 
-import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class StatsFragment extends Fragment {
 
+    public StatsFragment() {
+
+    }
+
+    @Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_stats, container, false);
+    }
 }
